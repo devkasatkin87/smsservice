7cdcc52c4c7285134c6adc976295ec19f777ebc6
kasatkin <kasatkin87@gmail.com>
2018-10-25 11:48:27 +0000

    Added class  SoapSmsGateWay.class

--- client.php
+++ client.php
@@ -41,13 +41,29 @@ class Request{
 }
 
 // создаем объект для отправки на сервер
-$req = new Request();
-$req->messageList = new MessageList();
-$req->messageList->message = new Message();
-$req->messageList->message->phone = '79871234567';
-$req->messageList->message->text = 'Тестовое сообщение 1';
-$req->messageList->message->date = '2013-07-21T15:00:00.26';
-$req->messageList->message->type = 15;
+$msg1 = new Message();
+$msg1->phone = '79871234567';
+$msg1->text = 'Тестовое сообщение 1';
+$msg1->date = '2013-07-21T15:00:00.26';
+$msg1->type = 15;
+
+$msg2 = new Message();
+$msg2->phone = '79871234567';
+$msg2->text = 'Тестовое сообщение 2';
+$msg2->date = '2014-08-22T16:01:10';
+$msg2->type = 16;
+
+$msg3 = new Message();
+$msg3->phone = '79871234567';
+$msg3->text = 'Тестовое сообщение 3';
+$msg3->date = '2014-08-22T16:01:10';
+$msg3->type = 17;
+
+$req->messageList[] = $msg1;
+$req->messageList[] = $msg2;
+$req->messageList[] = $msg3;
+
+$req->messageList = (object)$req->messageList;
 
 $client = new SoapClient(   "https://soap-kasatkin.c9users.io//smsservice.wsdl.php",
                             array( 'soap_version' => SOAP_1_2));
